0ac87d36e1b3d14b154ba1d404aab2aad7837b2b
Hasanadnan <71681524+Hasanadnan@users.noreply.github.com>
2021-02-26 17:47:38 +0600

    add HMS Project

---
+++ contact.php
@@ -0,0 +1,97 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>HMS | Contact us</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<!--start-wrap-->
+
+			<!--start-header-->
+			<div class="header">
+				<div class="wrap">
+				<!--start-logo-->
+				<div class="logo">
+					<a href="index.html" style="font-size: 30px;">Hospital Management system</a>
+				</div>
+				<!--end-logo-->
+				<!--start-top-nav-->
+				<div class="top-nav">
+					<ul>
+						<li><a href="index.html">Home</a></li>
+
+						<li class="active"><a href="contact.php">contact</a></li>
+					</ul>
+				</div>
+				<div class="clear"> </div>
+				<!--end-top-nav-->
+			</div>
+			<!--end-header-->
+		</div>
+		    <div class="clear"> </div>
+		   <div class="wrap">
+		   	<div class="contact">
+		   	<div class="section group">
+				<div class="col span_1_of_3">
+
+      			<div class="company_address">
+				     	<h2>Hospital Address  :</h2>
+						    	<p>Rawshan Ara Tower, House-06, Block-B, Section-01,</p>
+						   		<p> Kalwalipara, Mirpur-01 Dhaka, 1216,</p>
+						   		<p>Bangladesh</p>
+				   		<p>Phone:(+88) 01766-662888</p>
+				   		<p>Fax: (000) 000 00 00 0</p>
+				 	 	<p>Email: <span>labaidhms@gmail.com</span></p>
+
+				   </div>
+				</div>
+				<div class="col span_2_of_3">
+				  <div class="contact-form">
+				  	<h2>Contact Us</h2>
+					    <form>
+					    	<div>
+						    	<span><label>NAME</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>E-MAIL</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						     	<span><label>MOBILE.NO</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>SUBJECT</label></span>
+						    	<span><textarea> </textarea></span>
+						    </div>
+						   <div>
+						   		<span><input type="submit" value="Submit"></span>
+						  </div>
+					    </form>
+				    </div>
+  				</div>
+			  </div>
+			  	 <div class="clear"> </div>
+	</div>
+	<div class="clear"> </div>
+			</div>
+	      <div class="clear"> </div>
+		   <div class="footer">
+		   	 <div class="wrap">
+		   	<div class="footer-left">
+		   			<ul>
+						<li><a href="index.html">Home</a></li>
+
+						<li><a href="contact.php">contact</a></li>
+					</ul>
+		   	</div>
+
+		   	<div class="clear"> </div>
+		   </div>
+		   </div>
+		<!--end-wrap-->
+	</body>
+</html>
---
+++ hms/get_doctor.php
@@ -0,0 +1,58 @@
+<?php
+include('include/config.php');
+if(!empty($_POST["specilizationid"])) 
+{
+
+ $sql=mysql_query("select doctorName,id from doctors where specilization='".$_POST['specilizationid']."'");?>
+ <option selected="selected">Select Doctor </option>
+ <?php
+ while($row=mysql_fetch_array($sql))
+ 	{?>
+  <option value="<?php echo htmlentities($row['id']); ?>"><?php echo htmlentities($row['doctorName']); ?></option>
+  <?php
+}
+}
+
+
+if(!empty($_POST["doctor"])) 
+{
+
+ $sql=mysql_query("select docFees from doctors where id='".$_POST['doctor']."'");
+ while($row=mysql_fetch_array($sql))
+ 	{?>
+ <option value="<?php echo htmlentities($row['docFees']); ?>"><?php echo htmlentities($row['docFees']); ?></option>
+  <?php
+}
+
+
+
+
+}
+
+
+
+
+if(!empty($_POST["doctor"])) 
+{
+
+
+$query=mysql_query("select day from dr_timetbl where id='".$_POST['doctor']."'");
+ while($row=mysql_fetch_array($query))
+ 	{?>
+ <option value="<?php echo htmlentities($row['day']); ?>"><?php echo htmlentities($row['day']); ?></option>
+  <?php
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+?>
+
